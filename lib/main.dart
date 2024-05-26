e4a9dff8775235fa33c2b3f3e47ad7b134a2ce87
enriquegarcia96 <enriquegarcia259@yahoo.es>
2024-05-25 22:54:21 -0600

    feat: 🎸 comenzando en la nueva aplicacion
    nueva aplicacion para aprender flutter

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/theme/app_theme.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yes No app',
+      theme: AppTheme(selectedColor: 6).theme(),
+      debugShowCheckedModeBanner:false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body:  Center(
+          child: FilledButton.tonal(
+              onPressed: () {}, 
+              child: const Text('clic me') 
+          )
+        ),
+      ),
+    );
+  }
+}
