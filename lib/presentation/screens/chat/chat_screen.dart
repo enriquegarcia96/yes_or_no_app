f93beb1f7033b29eafc6f6390ad75506553f234a
enriquegarcia96 <enriquegarcia259@yahoo.es>
2024-05-27 16:48:31 -0600

    refactor: 💡 mensajes tipo burbujar
    burbujas de mensajes

--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_buble.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -40,15 +41,13 @@ class _ChatView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 10),
         child: Column(
           children: [
-            
-            Expanded(child: ListView.builder(
+            Expanded(
+                child: ListView.builder(
               itemCount: 100,
               itemBuilder: (context, index) {
-                return Text('Indice: $index');
+                return const MyMessageBuble();
               },
             )),
-            
-            
             Text('Mundo')
           ],
         ),
