1999ece996c4b49a0a158ea953313329c819c75d
enriquegarcia96 <enriquegarcia259@yahoo.es>
2024-06-04 11:38:52 -0600

    refactor: 💡 comportamiento del formField
    aprendiendo el comportamiento para los formularios de inputs

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -6,13 +6,14 @@ class MessageFielBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textController = TextEditingController();
+    final focusNode = FocusNode();
 
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
-        hintText: 'End your message with a "??"',
+        hintText: 'End your message with a "?"',
         enabledBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
@@ -20,15 +21,22 @@ class MessageFielBox extends StatelessWidget {
           onPressed: () {
             final textValue = textController.value.text;
             print('button : $textValue');
+            textController.clear();
           },
-        ));
+        ),
+        );
 
     return TextFormField(
+        onTapOutside: (event) {
+          focusNode.unfocus();
+        },
+        focusNode: focusNode,
         controller: textController,
         decoration: inputDecoration,
         onFieldSubmitted: (value) {
           print('Submit value $value');
           textController.clear();
+          focusNode.requestFocus();
         });
   }
 }
