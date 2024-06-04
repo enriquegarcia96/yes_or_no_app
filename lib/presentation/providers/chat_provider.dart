cdf02770630f9f98454f6aaa1d9c29e3eede60c8
enriquegarcia96 <enriquegarcia259@yahoo.es>
2024-06-04 12:44:40 -0600

    refactor: 💡 provider gestor de estado
    aprendiendo el gestor de estado

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+// ChangeNotifier => notifica cuando hayan cambios
+class ChatProvider extends ChangeNotifier {
+
+  List<Message> message = [
+    Message(text: 'Hola Amor!', fromWho: FromWho.me),
+    Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me)
+  ];
+
+
+  Future<void> sendMessage(String text) async {
+    // TODO: implementar metodo
+  }
+
+
+
+}
