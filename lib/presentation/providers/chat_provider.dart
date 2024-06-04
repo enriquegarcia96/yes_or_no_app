8aeab26b6bf2231ea92d472c5342fc8ff7f1b0cd
enriquegarcia96 <enriquegarcia259@yahoo.es>
2024-06-04 13:12:32 -0600

    refactor: 💡 MOSTRANDO mensajes del provider

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -4,9 +4,9 @@ import 'package:yes_no_app/domain/entities/message.dart';
 // ChangeNotifier => notifica cuando hayan cambios
 class ChatProvider extends ChangeNotifier {
 
-  List<Message> message = [
+  List<Message> messagesList = [
     Message(text: 'Hola Amor!', fromWho: FromWho.me),
-    Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me)
+    Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
   ];
 
 
--- lib/presentation/screens/chat/chat_screen.dart
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_message_buble.dart';
 import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
@@ -38,6 +41,8 @@ class _AppBar extends StatelessWidget implements PreferredSizeWidget {
 class _ChatView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final chatProvider = context.watch<ChatProvider>();
+
     return SafeArea(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 10),
@@ -45,11 +50,16 @@ class _ChatView extends StatelessWidget {
           children: [
             Expanded(
                 child: ListView.builder(
-              itemCount: 100,
+              itemCount: chatProvider.messagesList.length,
               itemBuilder: (context, index) {
-                return  (index % 2 == 0) ? const HerMessageBubble() : const MyMessageBuble();
+                final message = chatProvider.messagesList[index];
+
+                return (message.fromWho == FromWho.hers)
+                    ? const HerMessageBubble()
+                    : const MyMessageBuble();
               },
-            )),
+            ),
+            ),
 
             /// Caja de Texto de mensajes
             const MessageFielBox(),
