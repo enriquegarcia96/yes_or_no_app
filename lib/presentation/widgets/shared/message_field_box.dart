a81f7eb7ee0b94112f0d54f33967ca4eec211928
enriquegarcia96 <enriquegarcia259@yahoo.es>
2024-06-04 11:26:40 -0600

    refactor: 💡 borrando lineas inecesarias

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,28 +5,30 @@ class MessageFielBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textController = TextEditingController();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: const BorderSide(color: Colors.transparent),
         borderRadius: BorderRadius.circular(40));
 
     final inputDecoration = InputDecoration(
+        hintText: 'End your message with a "??"',
         enabledBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
           onPressed: () {
-            print('Valor de ñla caja de texto?');
+            final textValue = textController.value.text;
+            print('button : $textValue');
           },
         ));
 
     return TextFormField(
-      decoration: inputDecoration,
-      onFieldSubmitted: (value) {
-        print('Sumit value $value');
-      },
-      onChanged: (value) {
-        print('Chaged $value');
-      },
-    );
+        controller: textController,
+        decoration: inputDecoration,
+        onFieldSubmitted: (value) {
+          print('Submit value $value');
+          textController.clear();
+        });
   }
 }
